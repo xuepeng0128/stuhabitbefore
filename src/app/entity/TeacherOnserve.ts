15beac24fe906feadf31ae0658bfd82f195fdf47
yxpmatrix <2426574593@qq.com>
2019-04-01 22:36:13 +0800

    新建项目

--- src/app/entity/TeacherOnserve.ts
+++ src/app/entity/TeacherOnserve.ts
@@ -1,15 +1,16 @@
 import {School} from './School';
+import {TeacherDuty} from './TeacherDuty';
 
 export class TeacherOnserve {
   school: School; // 任教学校
   grade: number;  // 年纪
   classes: number; // 班级
-  rank: string ; // 职务
+  teacherDuty: TeacherDuty ; // 职务
 
-  constructor(options: {school?: School, grade?: number, classes?: number, rank?: string}= {}) {
+  constructor(options: {school?: School, grade?: number, classes?: number, teacherDuty?: TeacherDuty}= {}) {
     this.school = options.school;
     this.grade = options.grade;
     this.classes = options.classes;
-    this.rank = options.rank;
+    this.teacherDuty = options.teacherDuty;
   }
 }
